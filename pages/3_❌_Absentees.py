f5abf2cf5d16e26f6ac65ad523ddf5361bc5e506
SP-SuryaDEV <123246343+SP-SuryaDEV@users.noreply.github.com>
2024-07-08 15:12:46 +0530

    Update 3_❌_Absentees.py

--- pages/3_❌_Absentees.py
+++ pages/3_❌_Absentees.py
@@ -17,9 +17,10 @@ if st.session_state.get('data_option'):
         st.session_state.data = pd.read_csv('w397.csv')
     elif st.session_state.data_option == 'Leetcode Weekly Contest - 405 [07.07.2024]':
         st.session_state.data = pd.read_csv('w405.csv')
-    elif st.session_state.data_option == 'Leetcode Biweekly Contest - 130 [11.05.2024]':
+    elif st.session_state.data_option == 'Leetcode Biweekly Contest - 134 [06.07.2024]':
         st.session_state.data = pd.read_csv('bw134.csv')
     st.sidebar.header(st.session_state.data_option)
+    
     data = st.session_state.data
     
     #st.set_page_config(layout="wide")
