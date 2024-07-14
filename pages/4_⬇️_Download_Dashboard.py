d6e2a43e370b24d66ce09b8845b879aeea629b80
SP-SuryaDEV <123246343+SP-SuryaDEV@users.noreply.github.com>
2024-07-14 10:56:17 +0530

    Update 4_⬇️_Download_Dashboard.py

--- pages/4_⬇️_Download_Dashboard.py
+++ pages/4_⬇️_Download_Dashboard.py
@@ -8,6 +8,7 @@ import io
 from PIL import Image
 
 st.session_state.data_option = st.sidebar.selectbox(label='Select Contest Name', options=[
+    'Leetcode Weekly Contest - 406 [14.07.2024]',
     'Leetcode Weekly Contest - 405 [07.07.2024]',
     'Leetcode Biweekly Contest - 134 [06.07.2024]',
     'Leetcode Biweekly Contest - 130 [11.05.2024]',
@@ -24,6 +25,10 @@ if st.session_state.get('data_option'):
         st.session_state.data = pd.read_csv('w405.csv')
     elif st.session_state.data_option == 'Leetcode Biweekly Contest - 134 [06.07.2024]':
         st.session_state.data = pd.read_csv('bw134.csv')
+    elif st.session_state.data_option == 'Leetcode Weekly Contest - 406 [14.07.2024]':
+        st.session_state.data = pd.read_csv('w406.csv')
+
+    
     st.sidebar.header(st.session_state.data_option)
 
     # Load data
