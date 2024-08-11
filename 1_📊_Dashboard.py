eb54818049688745d59b263055c515cba35d364b
SP-SuryaDEV <123246343+SP-SuryaDEV@users.noreply.github.com>
2024-08-11 14:23:16 +0530

    Update 1_📊_Dashboard.py

--- 1_📊_Dashboard.py
+++ 1_📊_Dashboard.py
@@ -8,6 +8,7 @@ st.set_page_config(
     layout="wide")
 
 st.session_state.data_option = st.sidebar.selectbox(label='Select Contest Name', options=[
+    'Leetcode Weekly Contest - 410 [11.08.2024]',
     'Leetcode Weekly Contest - 409 [04.08.2024]',
     'Leetcode Biweekly Contest - 136 [03.08.2024]',
     'Leetcode Weekly Contest - 408 [28.07.2024]',
@@ -39,6 +40,9 @@ if st.session_state.get('data_option'):
         st.session_state.data = pd.read_csv('w409.csv')
     elif st.session_state.data_option == 'Leetcode Biweekly Contest - 136 [03.08.2024]':
         st.session_state.data = pd.read_csv('bw136.csv')
+    elif st.session_state.data_option == 'Leetcode Weekly Contest - 410 [11.08.2024]':
+        st.session_state.data = pd.read_csv('w410.csv')
+            
     st.sidebar.header(st.session_state.data_option)
     
     data = st.session_state.data
